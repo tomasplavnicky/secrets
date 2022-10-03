3f5248eee5b9bc75780c5cc3722607a315f5cd11
Tomas <tomas.plavnicky@gmail.com>
2022-10-03 09:43:22 +0200

    Test add with ssh

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -19,7 +22,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: [ 'password' ] });
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: [ 'password' ] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -36,32 +39,39 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res){
-  User.findOne({email: req.body.username}, function(err, foundUser){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
     } else {
       if(foundUser){
-        if(foundUser.password === req.body.password) {
-          res.render("secrets");
-        } else {
-          console.log("Wrong password");
-          res.render("login");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result == true) {
+            res.render("secrets")
+          } else {
+              console.log("Wrong password");
+              res.render("login");
+          }
+        })
       } else {
         console.log("User not found");
         res.render("home");
